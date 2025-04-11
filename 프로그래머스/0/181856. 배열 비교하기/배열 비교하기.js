df2c8f7c38343055c683c1f4ae96dff29f6180ba
Il-Woo-Choi <chldlfdn0440@naver.com>
2025-04-11 14:41:59 +0900

    [level 0] Title: 배열 비교하기, Time: 0.10 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/181856. 배열 비교하기/배열 비교하기.js
+++ 프로그래머스/0/181856. 배열 비교하기/배열 비교하기.js
@@ -1,23 +1,15 @@
 function solution(arr1, arr2) {
-    let num1 = 0;
-    let num2 = 0;
-    
     if(arr1.length > arr2.length){
         return 1;
     } else if(arr1.length < arr2.length){
         return -1;
     } else {
-        for(let i = 0; i < arr1.length; i++){
-            num1 += arr1[i];
-        }
+        const sum1 = arr1.reduce((acc1, num1) => acc1 + num1, 0);
+        const sum2 = arr2.reduce((acc2, num2) => acc2 + num2, 0);
         
-        for(let i = 0; i < arr2.length; i++){
-            num2 += arr2[i];
-        }
-        
-        if(num1 > num2){
+        if(sum1 > sum2){
             return 1;
-        } else if(num1 < num2) {
+        } else if(sum1 < sum2) {
             return -1;
         } else{
             return 0;
